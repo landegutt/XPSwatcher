8a8adac0b37e8a60aee938107f99c85ecec6ec18
Ove Lande <landegutt@hotmail.com>
2026-08-18 12:29:48 +0200

    Fix indentation

--- watch_calendar.py
+++ watch_calendar.py
@@ -159,7 +159,7 @@ def main():
 
         print("Ingen endringer funnet.")
 
-  send_push("🚀 Test fra GitHub XPS Watcher")
+    send_push("🚀 Test fra GitHub XPS Watcher")
 
     save_state(current_events)
 
